7c6e65bf190af14986e175c43b4397c7584b52bd
LinSFa <kevin.vandervaeren@ulb.be>
2026-03-04 18:44:54 +0100

    add parametriser version of float

--- src/float.cu
+++ src/float.cu
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>  
+#include <CLI/CLI.hpp>
 
 using std::rand;
 
@@ -35,67 +36,97 @@ __global__ void float_bitwiseXOR_kernel(float* c, const float* a, const float* b
         c[idx] = bin_c.f;
     }
 }
-#define N_THREAD 1
+
+enum class metric {
+    avg,
+    mean,
+};
+
+void main_kernel(int pre_array_size, int tsize, metric metric_choice, int num_iterations, char* output_filename) {
+    std::ofstream output_file(output_filename);
+    for (int _array_size = 1; _array_size <= pre_array_size; _array_size++) {
+
+        const int array_size = _array_size;
+        float* dev_a = 0, * dev_b = 0, * dev_c = 0;
+        float host_a[array_size], host_b[array_size], host_c[array_size];
+        dim3 block_size((array_size + tsize - 1) / tsize);
+        dim3 thread_size(tsize);
+        cudaEvent_t start_gpu, end_gpu;
+        cudaEventCreate(&start_gpu);
+        cudaEventCreate(&end_gpu);
+
+        float gpu_runtimes[num_iterations] = { 0 };
+
+
+        for (int iter = 0; iter < num_iterations; iter++) {
+            for (int i = 0; i < array_size; i++) {
+                host_a[i] = rand();
+                host_b[i] = rand();
+            }
+
+            CHK(cudaSetDevice(0));
+            CHK(cudaMalloc((void**)&dev_c, array_size * sizeof(float)));
+            CHK(cudaMalloc((void**)&dev_a, array_size * sizeof(float)));
+            CHK(cudaMalloc((void**)&dev_b, array_size * sizeof(float)));
+
+            CHK(cudaMemcpy(dev_a, host_a, array_size * sizeof(float), cudaMemcpyHostToDevice));
+            CHK(cudaMemcpy(dev_b, host_b, array_size * sizeof(float), cudaMemcpyHostToDevice));
+
+            cudaEventRecord(start_gpu);
+            float_bitwiseXOR_kernel << <block_size, thread_size >> > (dev_c, dev_a, dev_b, array_size);
+            cudaEventRecord(end_gpu);
+
+            CHK(cudaGetLastError());
+
+            // cudaDeviceSynchronize waits for the kernel to finish, and returns
+            // any errors encountered during the launch.
+            CHK(cudaDeviceSynchronize());
+            CHK(cudaMemcpy(host_c, dev_c, array_size * sizeof(float), cudaMemcpyDeviceToHost));
+
+            // Make sure the stop_gpu event is recorded before doing the time computation
+            cudaEventSynchronize(end_gpu);
+            cudaEventElapsedTime(&gpu_runtimes[iter], start_gpu, end_gpu);
+
+        Error:
+            cudaFree(dev_c);
+            cudaFree(dev_a);
+            cudaFree(dev_b);
+
+            // cudaDeviceReset must be called before exiting in order for profiling and
+            // tracing tools such as Nsight and Visual Profiler to show complete traces.
+            if (cudaDeviceReset() != cudaSuccess)
+            {
+                output_file << "cudaDeviceReset failed!\n";
+            }
+        }
+        float avg_gpu_runtime = 0.0f;
+        for (int iter = 0; iter < num_iterations; iter++) {
+            avg_gpu_runtime += gpu_runtimes[iter];
+        }
+        avg_gpu_runtime /= num_iterations;
+        output_file << array_size << "," << avg_gpu_runtime << "\n";
+    }
+    output_file.close();
+}
+
 
 int main(int argc, char** argv) {
-    const int array_size = 1;
-    float* dev_a = 0, * dev_b = 0, * dev_c = 0;
-    float host_a[array_size], host_b[array_size], host_c[array_size];
-    float gpu_runtime_ms = 0.0f;
-    char filename[] = "float_output.txt";
-    std::ofstream output_file(filename);
+    CLI::App app{ "Float Bitwise XOR" };
+    int pre_array_size{ 1 };
+    app.add_option("-n, --number", pre_array_size, "upper bound on the array size(default is 1)")->check(CLI::PositiveNumber);
 
-    dim3 block_size(array_size + (N_THREAD - 1) / N_THREAD);
-    dim3 thread_size(N_THREAD);
+    int thread_size{ 1024 };
+    app.add_option("-t, --thread", thread_size, "number of threads per block (default is 1024)")->check(CLI::PositiveNumber);
+    // metric metric_choice = metric::avg;
+    // app.add_option("-m, --metric", metric_choice, "metric to use for performance measurement (avg or mean)")->check(CLI::IsMember({ "avg", "mean" }));
 
-    for (int i = 0; i < array_size; i++) {
-        host_a[i] = rand();
-        host_b[i] = rand();
-    }
+    int num_iterations{ 1 };
+    app.add_option("-i, --iterations", num_iterations, "number of iterations to run for performance measurement");
 
-    cudaError_t cuda_status;
-    cudaEvent_t start_gpu, end_gpu;
-    cudaEventCreate(&start_gpu);
-    cudaEventCreate(&end_gpu);
+    std::string output_filename = "float_output.txt";
+    app.add_option("-o, --output", output_filename, "output file name for performance results");
 
-    CHK(cudaSetDevice(0));
-    CHK(cudaMalloc((void**)&dev_c, array_size * sizeof(float)));
-    CHK(cudaMalloc((void**)&dev_a, array_size * sizeof(float)));
-    CHK(cudaMalloc((void**)&dev_b, array_size * sizeof(float)));
-
-    CHK(cudaMemcpy(dev_a, host_a, array_size * sizeof(float), cudaMemcpyHostToDevice));
-    CHK(cudaMemcpy(dev_b, host_b, array_size * sizeof(float), cudaMemcpyHostToDevice));
-
-    cudaEventRecord(start_gpu);
-    float_bitwiseXOR_kernel << <block_size, thread_size >> > (dev_c, dev_a, dev_b, array_size);
-    cudaEventRecord(end_gpu);
-
-    CHK(cudaGetLastError());
-
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    CHK(cudaDeviceSynchronize());
-    CHK(cudaMemcpy(host_c, dev_c, array_size * sizeof(float), cudaMemcpyDeviceToHost));
-
-    // Make sure the stop_gpu event is recorded before doing the time computation
-    cudaEventSynchronize(end_gpu);
-    cudaEventElapsedTime(&gpu_runtime_ms, start_gpu, end_gpu);
-    output_file << "GPU runtime: " << gpu_runtime_ms << " ms\n";
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cuda_status = cudaDeviceReset();
-    if (cuda_status != cudaSuccess)
-    {
-        output_file << "cudaDeviceReset failed!  Do you have a CUDA-capable GPU installed?\n";
-    }
-
-    output_file.close();
+    CLI11_PARSE(app, argc, argv);
+    main_kernel(pre_array_size, thread_size, metric::avg, num_iterations, output_filename.data());
     return 0;
-
 }
